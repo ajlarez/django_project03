cb0b12ab6ce66afd1dacb74f4020c0bf96a70e33
Anthony larez <ajlarezm@gmail.com>
2025-03-15 11:15:23 -0400

    add post app

--- post/views.py
+++ post/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from post.models import Post
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post.html'
+
