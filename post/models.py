143ccadb37a4fa5d5769dd4237ee968c8df9e26f
Anthony larez <ajlarezm@gmail.com>
2025-03-15 12:06:42 -0400

    images

--- post/models.py
+++ post/models.py
@@ -2,16 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-<<<<<<< HEAD
-=======
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    #image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to='images/', blank=True)
     
     def __str__(self):
         return self.title
     
->>>>>>> feature/post
